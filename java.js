0c1cd18949d1dba6ab32832702fac1de9a84c593
Danielkombou <danielkombou7@gmail.com>
2024-03-05 13:12:55 +0100

    About to host

---
+++ java.js
@@ -0,0 +1,22 @@
+
+
+//const num = 10
+//console.log(num)
+
+
+
+
+
+    for (let i = 0; i <= 20; i++) {
+      if (i % 2 == 0) {
+        console.log(i);
+      }
+    }
+    
+
+
+    let count = 0;
+    while (count < 5) {
+        console.log(count);
+    }
+    
